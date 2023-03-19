3e4fb08eae997a2ed1f11fb14595206c66ebeb0f
Artemijs Kuznecovs <93352173+ratdriftbot@users.noreply.github.com>
2023-03-19 20:17:35 +0000

    Test2

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,25 @@
 def build_heap(data):
     swaps = []
-    z=len(data)
+    n=len(data)
 
-    for i in range(z // 2-1, -1, -1):
+    for i in range(n // 2-1, -1, -1):
         maks_indeks =i
         kreisais_berns=2*i+1
         labais_berns = 2*i+2
 
-        if kreisais_berns < z and data[kreisais_berns] < data[maks_indeks]:
+        if kreisais_berns < n and data[kreisais_berns] < data[maks_indeks]:
             maks_indeks = kreisais_berns
-        if labais_berns < z and data[labais_berns] < data[maks_indeks]:
+        if labais_berns < n and data[labais_berns] < data[maks_indeks]:
             maks_indeks=labais_berns
 
         if i!=maks_indeks:
             swaps.append((i, maks_indeks))
             data[i], data[maks_indeks]=data[maks_indeks], data[i]
 
-            while maks_indeks* 2+1 < z:
+            while maks_indeks* 2+1 < n:
                 i=maks_indeks
                 maks_indeks= 2*i+1
-                if maks_indeks+1 < z and data [maks_indeks+1] < data[maks_indeks]:
+                if maks_indeks+1 < n and data [maks_indeks+1] < data[maks_indeks]:
                     maks_indeks = maks_indeks+1
 
                 if data[i]> data[maks_indeks]:
@@ -27,10 +27,10 @@ def build_heap(data):
                     data[i], data[maks_indeks]=data[maks_indeks], data[i]
         else:
             j=maks_indeks
-            while j*2 +1 < z:
+            while j*2 +1 < n:
                 i=j
                 j=2*i+1
-                if j+1< z and data [j+1]<data[j]:
+                if j+1< n and data [j+1]<data[j]:
                     j=j+1
 
                 if data[i] > data[j]:
@@ -44,10 +44,10 @@ def build_heap(data):
 
 
 def main():
-    z = int(input())
+    n = int(input())
     data = list(map(int, input().split()))
 
-    assert len(data) == z
+    assert len(data) == n
 
    
     
